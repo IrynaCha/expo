ea3c1fac02c3c4a4a1db65b6928a12dd7e1b6a04
Iryna Kurnosova <kurnosova.iryna@gmail.com>
2019-02-17 22:16:05 +0200

    initial file

---
+++ src/com/iru/dao/DaoException.java
@@ -0,0 +1,17 @@
+package com.iru.dao;
+
+public class DaoException extends RuntimeException {
+    public static final long serialVersionUID = 1L;
+
+    public DaoException(String message) {
+        super(message);
+    }
+
+    public DaoException(Throwable cause) {
+        super(cause);
+    }
+
+    public DaoException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
