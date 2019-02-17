8f594effff3ac74530058d761d95c3385d37624a
Iryna Kurnosova <kurnosova.iryna@gmail.com>
2019-02-17 21:40:56 +0200

    DepaDomain without changes -> initial file

---
+++ src/com/iru/domain/DepaDomain.java
@@ -0,0 +1,99 @@
+package com.iru.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class DepaDomain {
+    private int id;
+    private String name;
+    private String abbreviation;
+    private List<GrouDomain> groups = new ArrayList<>();
+    private List<TeachDomain> teachDomains = new ArrayList<>();
+    private List<SubjDomain> subjDomains = new ArrayList<>();
+
+    public DepaDomain() {
+    }
+
+    public DepaDomain(int id, String name, String abbreviation, List<GrouDomain> groups, List<TeachDomain> teachDomains,
+                      List<SubjDomain> subjDomains) {
+        this.id = id;
+        this.name = name;
+        this.abbreviation = abbreviation;
+        this.groups = groups;
+        this.teachDomains = teachDomains;
+        this.subjDomains = subjDomains;
+    }
+
+    public DepaDomain(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAbbreviation() {
+        return abbreviation;
+    }
+
+    public void setAbbreviation(String abbreviation) {
+        this.abbreviation = abbreviation;
+    }
+
+    public List<GrouDomain> getGroups() {
+        return groups;
+    }
+
+    public void setGroups(List<GrouDomain> groups) {
+        this.groups = groups;
+    }
+
+    public List<TeachDomain> getTeachDomains() {
+        return teachDomains;
+    }
+
+    public void setTeachDomains(List<TeachDomain> teachDomains) {
+        this.teachDomains = teachDomains;
+    }
+
+    public List<SubjDomain> getSubjDomains() {
+        return subjDomains;
+    }
+
+    public void setSubjDomains(List<SubjDomain> subjDomains) {
+        this.subjDomains = subjDomains;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        DepaDomain that = (DepaDomain) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "DepaDomain [id=" + id + ", name=" + name + ", abbreviation=" + abbreviation + "]";
+    }
+}
