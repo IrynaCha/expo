c9bab806d151468ad1e87d1b0d00e7c98c620977
IrynaCha <kurnosova.iryna@gmail.com>
2019-02-21 12:09:44 +0200

    "DaoException.java" - adding new constructor

--- src/com/iru/dao/DaoException.java
+++ src/com/iru/dao/DaoException.java
@@ -3,6 +3,9 @@ package com.iru.dao;
 @SuppressWarnings("serial")
 public class DaoException extends Exception {
 
+    public DaoException() {
+    }
+
     public DaoException(String message) {
         super(message);
     }
