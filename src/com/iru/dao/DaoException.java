000e711efaac8a9b75f16ef1d59315eda75cf779
IrynaCha <kurnosova.iryna@gmail.com>
2019-02-23 08:39:55 +0200

    "DaoException.java" removed unnecessary items

--- src/com/iru/dao/DaoException.java
+++ src/com/iru/dao/DaoException.java
@@ -3,16 +3,8 @@ package com.iru.dao;
 @SuppressWarnings("serial")
 public class DaoException extends Exception {
 
-    /*public DaoException() {
+    public DaoException() {
     }
-
-    public DaoException(String message) {
-        super(message);
-    }
-
-    public DaoException(Throwable cause) {
-        super(cause);
-    }*/
 
     public DaoException(String message, Throwable cause) {
         super(message, cause);
