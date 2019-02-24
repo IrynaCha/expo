de8fbfdff175f0ca103cf08fbba4c212237eadd4
IrynaCha <kurnosova.iryna@gmail.com>
2019-02-24 19:34:00 +0200

    DaoFactory.java - path to db.property and get info from file once

--- src/com/iru/dao/DaoFactory.java
+++ src/com/iru/dao/DaoFactory.java
@@ -2,39 +2,34 @@ package com.iru.dao;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
 public class DaoFactory {
-    private static final String PROPERTY_URL = "url";
-    private static final String PROPERTY_DRIVER = "driver";
-    private static final String PROPERTY_USERNAME = "username";
-    private static final String PROPERTY_PASSWORD = "password";
+    private static String url;
+    private static String user;
+    private static String password;
 
     public Connection getConnection() {
-        Properties properties = new Properties();
-        Path currentPath = Paths.get("");
-        String s = currentPath.toAbsolutePath().toString();
-        try {
-            properties.load(new FileInputStream(s + "\\resources\\db\\db.properties"));
-        } catch (IOException e) {
-            e.printStackTrace();
+        if (url == null || user == null || password == null) {
+            Properties properties = new Properties();
+            try {
+                properties.load(new FileInputStream("resources/db/db.properties"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            url = properties.getProperty("url");
+            user = properties.getProperty("username");
+            password = properties.getProperty("password");
         }
-
-        String url = properties.getProperty("url");
-        String user = properties.getProperty("username");
-        String password = properties.getProperty("password");
-
-        Connection con = null;
+        Connection connection = null;
         try {
-            con = DriverManager.getConnection(url, user, password);
+            connection = DriverManager.getConnection(url, user, password);
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return con;
+        return connection;
     }
 }
